6a4a3bad7d549a1be7b8daf92157655f31377f77
Spieler2 <mlingroen@stud.hs-bremen.de>
2016-01-22 00:48:33 +0100

    Initiales Commit

---
+++ CompManager/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CompManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.loginButton = new MetroFramework.Controls.MetroButton();
+            this.passwordTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.cancelButton = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // usernameTextBox
+            // 
+            // 
+            // 
+            // 
+            this.usernameTextBox.CustomButton.Image = null;
+            this.usernameTextBox.CustomButton.Location = new System.Drawing.Point(216, 1);
+            this.usernameTextBox.CustomButton.Name = "";
+            this.usernameTextBox.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.usernameTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.usernameTextBox.CustomButton.TabIndex = 1;
+            this.usernameTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.usernameTextBox.CustomButton.UseSelectable = true;
+            this.usernameTextBox.CustomButton.Visible = false;
+            this.usernameTextBox.Lines = new string[] {
+        "Benutzername"};
+            this.usernameTextBox.Location = new System.Drawing.Point(23, 100);
+            this.usernameTextBox.MaxLength = 32767;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.PasswordChar = '\0';
+            this.usernameTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.usernameTextBox.SelectedText = "";
+            this.usernameTextBox.SelectionLength = 0;
+            this.usernameTextBox.SelectionStart = 0;
+            this.usernameTextBox.Size = new System.Drawing.Size(238, 23);
+            this.usernameTextBox.Style = MetroFramework.MetroColorStyle.Green;
+            this.usernameTextBox.TabIndex = 0;
+            this.usernameTextBox.Text = "Benutzername";
+            this.usernameTextBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.usernameTextBox.UseSelectable = true;
+            this.usernameTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.usernameTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(23, 178);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(134, 23);
+            this.loginButton.Style = MetroFramework.MetroColorStyle.Green;
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+            this.loginButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.loginButton.UseSelectable = true;
+            this.loginButton.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // passwordTextBox
+            // 
+            // 
+            // 
+            // 
+            this.passwordTextBox.CustomButton.Image = null;
+            this.passwordTextBox.CustomButton.Location = new System.Drawing.Point(216, 1);
+            this.passwordTextBox.CustomButton.Name = "";
+            this.passwordTextBox.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.passwordTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.passwordTextBox.CustomButton.TabIndex = 1;
+            this.passwordTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.passwordTextBox.CustomButton.UseSelectable = true;
+            this.passwordTextBox.CustomButton.Visible = false;
+            this.passwordTextBox.Lines = new string[] {
+        "Passwort"};
+            this.passwordTextBox.Location = new System.Drawing.Point(23, 140);
+            this.passwordTextBox.MaxLength = 32767;
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '\0';
+            this.passwordTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.passwordTextBox.SelectedText = "";
+            this.passwordTextBox.SelectionLength = 0;
+            this.passwordTextBox.SelectionStart = 0;
+            this.passwordTextBox.Size = new System.Drawing.Size(238, 23);
+            this.passwordTextBox.Style = MetroFramework.MetroColorStyle.Green;
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Text = "Passwort";
+            this.passwordTextBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.passwordTextBox.UseSelectable = true;
+            this.passwordTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.passwordTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(164, 178);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(97, 23);
+            this.cancelButton.Style = MetroFramework.MetroColorStyle.Green;
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Abbrechen";
+            this.cancelButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.cancelButton.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 227);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.usernameTextBox);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "CompManager";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox usernameTextBox;
+        private MetroFramework.Controls.MetroButton loginButton;
+        private MetroFramework.Controls.MetroTextBox passwordTextBox;
+        private MetroFramework.Controls.MetroButton cancelButton;
+    }
+}
+
---
+++ CompManager/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+
+namespace CompManager
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
