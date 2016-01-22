f4be0212bbb93acb1cd4f2ef77b73f2c24d77a35
Spieler2 <mlingroen@stud.hs-bremen.de>
2016-01-22 18:26:07 +0100

    Bisschen was überlegt

--- CompManager/UserValidation.cs
+++ CompManager/UserValidation.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CompManager
+/*TODO 
+Auf die Api zugreifen und die Daten verschlüsselt übermitteln
+oAuth
+Daten lokal sicher speichern
+    */
+public class UserValidation
 {
-    /*TODO 
-    Auf die Api zugreifen und die Daten verschlüsselt übermitteln
-    oAuth
-    Daten lokal sicher speichern
-        */
-    class UserValidation
+    private String username;
+    private String password;
+
+    public UserValidation(String _username, String _password)
     {
+        username = _username;
+        password = _password;
     }
+
+    private bool checkData()
+    {
+        /*
+            Die Daten gegen den Server abgleichen
+            Schlüssel auf dem Rechner sicher speichern
+
+            Wenn login erfolgreich, dann rückgabe von true sonst false;
+        */
+        return true;
+    }
+
+    private void safeSessionKey()
+    {
+        /*
+            Daten sicher verwahren!
+        */
+    }
+
 }
